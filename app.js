6161c77f934b602f71ef76e39ef517d7fcb3bb20
Raghava <busanaboinaraghava@gmail.com>
2023-10-26 18:58:03 +0530

    from mac

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-app.use(express.json({limit:'10mb'}))
+app.use(express.json({ limit: '10mb' }));
 require('./helpers/cors')(app);
 require('./helpers/db')();
 require('./helpers/redis');
 require('./helpers/routes')(app);
 require('./helpers/publisher');
 
-const port = process.env.PORT||5000;
+const port = process.env.PORT || 5000;
+console.log('REDIS_HOST', process.env.REDIS_HOST);
 app.listen(port, () => console.log(`Listening on port ${port}...`));
